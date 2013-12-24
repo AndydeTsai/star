a9da06c925f6f7e0c9be2abfa2002e7e23521964
AndydeTsai <stuandychen@gmail.com>
2013-12-24 23:34:54 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int i=0,j;
+    cout<<"Type a number"<<endl;
+    cin>>j;
+    for(i=0;i<j;i++){
+       cout<<"*"<<endl;              
+                     }
+    system("PAUSE");
+    return 0;
+}
